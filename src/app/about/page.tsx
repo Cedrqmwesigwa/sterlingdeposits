d9c4c8b4e02c4fa50960578fc259689ba7ff8b35
Cedrq Mwesigwa <mctyptys@gmail.com>
2025-06-18 05:48:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -33,7 +33,7 @@ const AboutPage = () => {
           <div className="grid md:grid-cols-2 gap-12 items-center">
             <div className="relative h-80 w-full rounded-lg overflow-hidden shadow-lg">
               <Image 
-                src="https://placehold.co/800x600.png" 
+                src="https://images.unsplash.com/photo-1580741753044-b3f303ad361b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxjb21tZXJjaWFsJTIwYnVpbGRpbmd8ZW58MHx8fHwxNzUwMTg2MTIzfDA&ixlib=rb-4.1.0&q=80&w=1080" 
                 alt="Sterling Contractors team working"
                 layout="fill"
                 objectFit="cover"
