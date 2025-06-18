8a51b842e2edb92d8e2bd2fcdb3f641001587d7e
Cedrq Mwesigwa <mctyptys@gmail.com>
2025-06-18 07:01:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-2xl

--- src/components/sections/ShopSection.tsx
+++ src/components/sections/ShopSection.tsx
@@ -8,7 +8,7 @@ const products: Product[] = [
   {
     id: 'cement-bag',
     name: 'Cement (50kg Bag)',
-    price: '42,000',
+    price: '32,000',
     unit: 'per bag',
     imageSrc: 'https://images.unsplash.com/photo-1473230841016-ca42011196d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxjZW1lbnQlMjBiYWd8ZW58MHx8fHwxNzUwMjI4NjM3fDA&ixlib=rb-4.1.0&q=80&w=1080',
     imageHint: 'cement bag',
